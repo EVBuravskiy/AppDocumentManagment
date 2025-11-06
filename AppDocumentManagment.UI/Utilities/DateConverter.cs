e89de3bd3c42698191dd9b4286386caf990f8771
EBuravskiy <elric0sphinx@gmail.com>
2025-11-06 10:06:42 +0500

    Implemented a date converter for DocumentRegistratrionWindow view

---
+++ AppDocumentManagment.UI/Utilities/DateConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AppDocumentManagment.UI.Utilities
+{
+    [ValueConversion(typeof(DateTime), typeof(String))]
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value != null)
+            {
+                if (value is DateTime date)
+                {
+                    return date.ToLongDateString();
+                }
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strValue = value as string;
+            DateTime resultDateTime;
+            if (DateTime.TryParse(strValue, out resultDateTime))
+            {
+                return resultDateTime;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
