a1d42f39e2dab60197f22ba0498678b17781ef0e
EBuravskiy <elric0sphinx@gmail.com>
2025-12-12 11:21:18 +0500

    Added entities to application context

--- AppDocumentManagment.DB/Controllers/ApplicationContext.cs
+++ AppDocumentManagment.DB/Controllers/ApplicationContext.cs
@@ -17,6 +17,10 @@ namespace AppDocumentManagment.DB.Controllers
         public DbSet<ExternalDocumentFile> ExternalDocumentFiles { get; set; } = null!;
         public DbSet<InternalDocument> InternalDocuments { get; set; } = null!;
         public DbSet<InternalDocumentFile> InternalDocumentFiles { get; set; } = null!;
+        public DbSet<ProductionSubTask> ProductionSubTasks { get; set; } = null!;
+        public DbSet<ProductionTaskComment> ProductionTaskComments { get; set; } = null!;
+        public DbSet<ProductionTaskFile> ProductionTaskFiles { get; set; } = null!;
+        public DbSet<ProductionTask> ProductionTasks { get; set; } = null!;
         public ApplicationContext() 
         {
             Database.EnsureCreated();
