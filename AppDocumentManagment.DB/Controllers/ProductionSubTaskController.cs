9bc54abfd5138efe5dc320ca9d6e44fecfd1e8a6
EBuravskiy <elric0sphinx@gmail.com>
2025-12-12 11:17:34 +0500

    Controllers have been developed for the task entity and its associated entities.

---
+++ AppDocumentManagment.DB/Controllers/ProductionSubTaskController.cs
@@ -0,0 +1,71 @@
+﻿using AppDocumentManagment.DB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDocumentManagment.DB.Controllers
+{
+    public class ProductionSubTaskController
+    {
+        public bool AddProductionSubTask(ProductionSubTask productionSubTask)
+        {
+            if (productionSubTask == null) return false;
+            bool result = false;
+            try
+            {
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    context.ProductionSubTasks.Add(productionSubTask);
+                    context.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! Ошибка в сохранении подзадачи в базу данных");
+                result = false;
+            }
+            return result;
+        }
+
+        public bool AddProductionSubTasks(List<ProductionSubTask> productionSubTasks)
+        {
+            if (productionSubTasks == null || productionSubTasks.Count == 0) return false;
+            bool result = false;
+            try
+            {
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    context.ProductionSubTasks.AddRange(productionSubTasks);
+                    context.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! Ошибка в сохранении подзадач в базу данных");
+                result = false;
+            }
+            return result;
+        }
+
+        public List<ProductionSubTask> GetProductionSubTasks(int productionTaskID)
+        {
+            List<ProductionSubTask> productionSubTasks = new List<ProductionSubTask>();
+            try
+            {
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    productionSubTasks = context.ProductionSubTasks.Where(s => s.ProductionTaskID == productionTaskID).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! Ошибка в получении подзадач из базы данных");
+            }
+            return productionSubTasks;
+        }
+    }
+}
---
+++ AppDocumentManagment.DB/Controllers/ProductionTaskController.cs
@@ -0,0 +1,91 @@
+﻿using AppDocumentManagment.DB.Models;
+
+namespace AppDocumentManagment.DB.Controllers
+{
+    public class ProductionTaskController
+    {
+        public bool AddProductionTask(ProductionTask productionTask)
+        {
+            if (productionTask == null) return false;
+            bool result = false;
+            try
+            {
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    ExternalDocument externalDocument = new ExternalDocument();
+                    if (productionTask.ExternalDocumentID != 0)
+                    {
+                        externalDocument = context.ExternalDocuments.SingleOrDefault(exd => exd.ExternalDocumentID == productionTask.ExternalDocumentID);
+                    }
+                    InternalDocument internalDocument = new InternalDocument();
+                    if (productionTask.InternalDocumentID != 0)
+                    {
+                        internalDocument = context.InternalDocuments.SingleOrDefault(ind => ind.InternalDocumentID == productionTask.InternalDocumentID);
+                    }
+                    if (externalDocument != null)
+                    {
+                        productionTask.ExternalDocument = externalDocument;
+                    }
+                    if (internalDocument != null)
+                    {
+                        productionTask.InternalDocument = internalDocument;
+                    }
+                    context.ProductionTasks.Add(productionTask);
+                    context.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! Ошибка в сохранении задачи в базу данных");
+                result = false;
+            }
+            return result;
+        }
+
+        public List<ProductionTask> GetProductionTasks()
+        {
+            List<ProductionTask> productionTasks = new List<ProductionTask>();
+            try
+            {
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    productionTasks = context.ProductionTasks.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! Ошибка в получении списка задач из базы данных");
+            }
+            return productionTasks;
+        }
+
+        public List<ProductionTask> GetProductionTasksByEmployeeID(int employeeID)
+        {
+            List<ProductionTask> allProductionTasks = new List<ProductionTask>();
+            List<ProductionTask> productionTasks = new List<ProductionTask>();
+            try
+            {
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    allProductionTasks = context.ProductionTasks.ToList();
+                    foreach (ProductionTask productionTask in allProductionTasks)
+                    {
+                        foreach (Employee employee in productionTask.Employees)
+                        {
+                            if (employee.EmployeeID == employeeID)
+                            {
+                                productionTasks.Add(productionTask);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! Ошибка в получении списка задач по идентификатору сотрудника");
+            }
+            return productionTasks;
+        }
+    }
+}
